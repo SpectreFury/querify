e3c1390b86f8977060b99ea97402846d1ce71725
Ayush Soni <soni.ayush.2212@gmail.com>
2023-12-23 13:24:47 +0530

    Tried to add form

--- app/(main)/dashboard/_components/Dashboard.tsx
+++ app/(main)/dashboard/_components/Dashboard.tsx
@@ -5,9 +5,19 @@ import {
   CardHeader,
   CardDescription,
   CardContent,
-  CardFooter
+  CardFooter,
 } from "@/components/ui/card";
 import { Button } from "@/components/ui/button";
+import { ScrollArea } from "@/components/ui/scroll-area";
+import {
+  Dialog,
+  DialogTitle,
+  DialogHeader,
+  DialogContent,
+  DialogDescription,
+  DialogTrigger,
+} from "@/components/ui/dialog";
+import { Input } from "@/components/ui/input";
 
 const Dashboard = () => {
   return (
@@ -20,10 +30,26 @@ const Dashboard = () => {
           </CardDescription>
         </CardHeader>
         <CardContent>
-          <p>CassandraDB</p>
+          <ScrollArea className="h-[200px]">
+            <div className="p-2 hover:bg-neutral-100 rounded flex justify-between items-center cursor-pointer">
+              <div className="">MongoDB</div>
+              <div className="text-muted-foreground text-sm">
+                Last Queried: 28 mins ago
+              </div>
+            </div>
+          </ScrollArea>
         </CardContent>
         <CardFooter>
-          <Button>Add a new database</Button>
+          <Dialog>
+            <DialogTrigger asChild>
+              <Button>Add a new database</Button>
+            </DialogTrigger>
+            <DialogContent>
+              <DialogHeader>
+                <DialogTitle>Add a new database</DialogTitle>
+              </DialogHeader>
+            </DialogContent>
+          </Dialog>
         </CardFooter>
       </Card>
     </div>
